609d147e66def17a99707e39669b2209185cd055
Liam <lijamurr@ucsc.edu>
2022-10-09 00:51:26 +0000

    working sine function

--- asgn2/mathlib.c
+++ asgn2/mathlib.c
@@ -35,7 +35,7 @@ int my_exponent(int base, int exponent){
 //Absolute Value Function
 double my_abs(double x){
 	double abs_x = x;
-	if (x>0) {}
+	if (x>=0) {}
 	else {abs_x=-abs_x;}
 	return abs_x;
 	} 
@@ -53,10 +53,37 @@ double my_sqrt(double x) {
     return value;
 }
 
+//Sine Function
+double my_sine(double x) {
+	double last_entry = x;
+	long double running_total = x;
+	long double EPSILON = 1e-10;
+	int counter_n = 1;
+	while (my_abs(last_entry) >= EPSILON) 
+		{
+		double term1 = x / (2.0 * counter_n);
+		double term2 = x / ((2.0 * counter_n) + 1);
+		double current_val = (last_entry * term1) * term2;
+		current_val = my_abs(current_val);
+		if (counter_n % 2 == 1 | counter_n == 1) 
+			{current_val = -current_val;}
+		else {}
+		printf("iteration: %d, value: %lf\n", counter_n,
+			current_val);
+		counter_n++;
+		running_total += current_val;
+		last_entry = current_val;
+		}
+	if (-0.0000000001 <= running_total && running_total <= 0.0000000001)
+		{running_total = my_abs(running_total);}
+	return running_total;
+}
+
 int main(void)
 {
-printf("the absolute value of %d is %f \n",-4,my_abs(-4.0));
-printf("the root of %d is %f \n",32,my_sqrt(32));
+printf("the sine of %f is %lf \n",(M_PI/2),my_sine(M_PI/2));
+printf("the sine of %f is %lf \n",(M_PI),my_sine(M_PI));
+printf("the sine of %f is %lf \n",(0.0),my_sine(0.0));
 return 0;
 }
 	
