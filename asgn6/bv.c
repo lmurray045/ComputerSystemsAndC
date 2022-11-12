d747296494912a5d1c81e216cc612019ba45e86e
Liam <lijamurr@ucsc.edu>
2022-11-12 23:17:44 +0000

    bv.c is done

--- asgn6/bv.c
+++ asgn6/bv.c
@@ -66,20 +66,35 @@ void bv_clr_bit(BitVector *bv, uint32_t i){
 	return;
 }
 
+//bit vector probe bit: returns the ith bit of the bit vector.
+uint8_t bv_get_bit(BitVector *bv, uint32_t i){
+	uint32_t i2 = i - 1;
+	uint64_t bit_vector_byte = i2 / 64;
+	uint64_t location_byte = i2 % 64;
+	uint64_t bitwise_num = pow(2, (location_byte - 1));
+	uint64_t bit_test = *(bv->vector+bit_vector_byte) & bitwise_num;
+	if(bit_test > 0){
+		return 1;
+		}
+	else{
+		return 0;
+		}
+}
+
 int main()
 {
 	BitVector* bv = bv_create(20);
 	printf("Vector Length: %d\n", bv_length(bv));
 	bv_set_bit(bv, 4);
+	printf("get bit 4: %d\n", bv_get_bit(bv, 4));
 	bv_print(bv);
 	bv_set_bit(bv, 6);
-	bv_print(bv);
-	bv_clr_bit(bv, 4);
+	printf("get bit 6: %d\n", bv_get_bit(bv, 6));
 	bv_print(bv);
 	bv_set_bit(bv, 20);
+	printf("get bit 20: %d\n", bv_get_bit(bv, 20));
 	bv_print(bv);
-	bv_clr_bit(bv, 6);
-	bv_print(bv);
-	bv_clr_bit(bv, 20);
-	bv_print(bv);
+	printf("get bit 18: %d\n", bv_get_bit(bv, 18));
+	printf("get bit 7: %d\n", bv_get_bit(bv, 7));
+	
 }
