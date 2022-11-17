99aa4a6495fb81e21d222f4c747716f2a6837bbc
Liam <lijamurr@ucsc.edu>
2022-11-17 10:06:02 +0000

    hashtable updated to have length and stat tracjing

--- asgn6/ht.c
+++ asgn6/ht.c
@@ -56,22 +56,76 @@ void ht_insert(HashTable *ht, char *oldspeak, char *newspeak){
 	if(*(ht->lists + addr) == NULL){
 		*(ht->lists + addr) = ll_create(ht->mtf);
 		ll_insert(*((ht->lists + addr)), oldspeak, newspeak);
+		ht->n_keys += 1;
 	}
-	else{
+	else{	
+		uint32_t temp = ll_length(*(ht->lists + addr));
 		ll_insert(*(ht->lists + addr), oldspeak, newspeak);
+		if((ll_length(*(ht->lists + addr))) != temp){
+			ht->n_keys += 1;
+		}
 	}
 	return;
 }
 
+//hash table lookup
+Node *ht_lookup(HashTable *ht, char *oldspeak){
+	uint64_t addr = hash(ht->salt, oldspeak) % ht->size;
+	if(*(ht->lists + addr) == NULL){
+		ht->n_misses += 1;
+		ht->n_examined = links;
+		return NULL;
+	}
+	Node* nd = ll_lookup(*(ht->lists + addr), oldspeak);
+	if(nd == NULL){
+		ht->n_misses += 1;
+		ht->n_examined = links;
+		return NULL;
+	}
+	ht->n_hits += 1;
+	ht->n_examined = links;
+	return nd;
+}
+
+//hash table size
+uint32_t ht_size(HashTable *ht){
+	return ht->size;
+}
+
+//hash table count: returns the number of initilzed linked list in the table
+uint32_t ht_count(HashTable *ht){
+	uint32_t counter = 0;
+	for(uint32_t i = 0; i < ht->size; i++){
+		if(*(ht->lists + i) != NULL){
+			counter++;
+		}
+	}
+	return counter;
+}
+
+
+
 int main(void){
 	HashTable * ht = ht_create(10, true);
 	printf("hash table created \n");
+	printf("hash table size: %u \n", ht_size(ht));
+	printf("hash table count: %u \n", ht_count(ht));
 	ht_insert(ht, "hello", "world");
 	ht_insert(ht, "whats u", "dog");
 	ht_insert(ht, "whts up", "dog");
 	ht_insert(ht, "whaup", "dog");
+	ht_insert(ht, "whaup", "dog");
 	ht_insert(ht, "whats", "dog");
+	node_print(ht_lookup(ht, "whaup"));
+	node_print(ht_lookup(ht, "whup"));
+	node_print(ht_lookup(ht, "whts up"));
 	ht_print(ht);
+	printf("hash table count: %u \n", ht_count(ht));
+	printf("\n");
+	printf("hash table keys: %u\n", ht->n_keys);
+	printf("hash table hits: %u\n", ht->n_hits);
+	printf("hash table misses: %u\n", ht->n_misses);
+	printf("hash table examined: %u\n", ht->n_examined);
 	return 0;
 }
 
