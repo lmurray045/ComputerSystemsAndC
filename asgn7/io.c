d203d90b6337ca6228e6e98c0c3fd4f6191838f6
Liam <lijamurr@ucsc.edu>
2022-12-01 21:31:58 +0000

    io is functional i think

--- asgn7/io.c
+++ asgn7/io.c
@@ -4,6 +4,7 @@
 #include <fcntl.h>
 #include "io.h"
 #include "defines.h"
+#include "code.h"
 #include <math.h>
 
 //I/O functions implementation
@@ -11,9 +12,15 @@
 //variable declaration
 uint64_t bytes_read = 0;
 uint64_t bytes_written = 0;
+
 static uint8_t bitvec[BLOCK];
 static uint64_t index = 0;
 static uint64_t bvsize = 0;
+
+static uint8_t wtbitvec[1];
+static uint64_t wtdex = 0;
+//static uint64_t wtsize = 0;
+
 
 //read bytes: function for reading bytes from a file
 int read_bytes(int infile, uint8_t *buf, int nbytes){
@@ -60,7 +67,7 @@ bool read_bit(int infile, uint8_t *bit){
 	}
 	uint64_t byte_address = index / 8;
 	uint64_t bit_address = index % 8;
-	bit_address = 7 - bit_address;
+	//bit_address = 7 - bit_address;
 	uint64_t bit_num = pow(2, bit_address);
 	if((bitvec[byte_address] & bit_num) > 0){
 		*bit = 1;
@@ -74,12 +81,88 @@ bool read_bit(int infile, uint8_t *bit){
 	return true;
 }
 
-int main(void){
-	uint8_t buf[100];
-	uint8_t *bufpoint = &buf[0];
+//repeatedly write codes into the buffer. When its full, send it to outfile.
+void write_code(int outfile, Code *c){
 	uint8_t bit;
-	int fd = open("example.txt", O_RDONLY);
-	printf("example.txt in binary: ");
+	for(uint64_t counter = 0; counter < c->top; counter++){
+		if(code_get_bit(c, counter) == true){
+			bit = 1;
+		}
+		else{
+			bit = 0;
+		}
+		uint64_t byte_address = wtdex / 8;
+		uint64_t bit_address = wtdex % 8;
+		uint64_t bit_num = pow(2, bit_address);
+		if(bit == 1){
+			wtbitvec[byte_address] = wtbitvec[byte_address] | bit_num;
+		}
+		else{
+			bit_num = ~bit_num;
+			wtbitvec[byte_address] = wtbitvec[byte_address] & bit_num;
+		}
+		printf("index: %lu\n", wtdex);
+		printf("buffer: %d\n", wtbitvec[0]);
+		wtdex++;
+		if(wtdex == (1 * 8)){
+			write_bytes(outfile, &wtbitvec[0], 1);
+			for(uint64_t i = 0; i < 1; i++){
+				wtbitvec[i] = 0;
+			}
+			wtdex = 0;
+		}
+	}
+	return;
+}
+
+//flush bits. Basically, send all the bits in the array that didnt make fill the buffer and get sent by write_codes()
+void flush_codes(int outfile){
+	uint64_t byte_address = wtdex / 8;
+	for(uint64_t i = 0; i <= byte_address; i++){
+		write_bytes(outfile, &wtbitvec[byte_address], 1);
+	}
+	return;
+}
+
+int main(void){
+	/*uint8_t buf[100];
+	uint8_t *bufpoint = &buf[0];
+	printf("\n");
+	printf("%d\n", counter);
+	//printf("bytes read: %d\n", read_bytes(fd, bufpoint, 100));
+	//printf("buffer contents: %s\n", buf);
+	close(fd);
+	*/
+	Code c1 = code_init();
+	code_push_bit(&c1, 1);
+	code_push_bit(&c1, 0);
+	code_push_bit(&c1, 1);
+	code_push_bit(&c1, 1);
+	code_push_bit(&c1, 0);
+	code_push_bit(&c1, 1);
+	Code c2 = code_init();
+	code_push_bit(&c2, 1);
+	code_push_bit(&c2, 0);
+	code_push_bit(&c2, 0);
+	code_push_bit(&c2, 1);
+	code_push_bit(&c2, 0);
+	code_push_bit(&c2, 1);
+	Code c3 = code_init();
+	code_push_bit(&c3, 0);
+	code_push_bit(&c3, 1);
+	code_push_bit(&c3, 1);
+	code_push_bit(&c3, 0);
+	code_push_bit(&c3, 1);
+	code_push_bit(&c3, 1);
+	int fdo = open("example2.txt", O_WRONLY | O_CREAT);
+	write_code(fdo, &c1);
+	write_code(fdo, &c2);
+	write_code(fdo, &c3);
+	flush_codes(fdo);
+	close(fdo);
+	uint8_t bit;
+	int fd = open("example2.txt", O_RDONLY);
+	printf("example2.txt in binary: ");
 	int counter;
 	for(counter = 1; counter != 500 && read_bit(fd, &bit) == true; counter++){
 		printf("%d", bit);
@@ -88,12 +171,5 @@ int main(void){
 		}
 	}
 	printf("\n");
-	printf("%d\n", counter);
-	//printf("bytes read: %d\n", read_bytes(fd, bufpoint, 100));
-	//printf("buffer contents: %s\n", buf);
-	close(fd);
-	int fdo = open("example2.txt", O_WRONLY | O_CREAT);
-	printf("bytes written: %d\n", write_bytes(fdo, bufpoint, 100));
-	close(fdo);
 	return 0;
 }
